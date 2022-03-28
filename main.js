944d938afd763172c360c1d6b48361e02c2dacc4
Christian Paulo Gomonit <xchan824@gmail.com>
2022-03-29 02:50:30 +0800

    added index.html, main.js and style.css

---
+++ main.js
@@ -0,0 +1,7 @@
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
