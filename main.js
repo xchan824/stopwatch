4ad020740f69657ca3bfce92ecb3370474e77279
Christian Paulo Gomonit <xchan824@gmail.com>
2022-03-30 02:17:13 +0800

    modified a lot of code on main.js

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
+window.onload = function () {
+  let milliseconds = 0;
+  let seconds = 0;
+  let minutes = 0;
 
-const startButton = document.getElementById('start');
-const stopButton = document.getElementById('stop');
-const resetButton = document.getElementById('reset');
+  let interval = 0;
+
+  const appendMilli = document.getElementById("milliseconds");
+  const appendSeconds = document.getElementById("seconds");
+  const appendMinutes = document.getElementById("minutes");
+
+  const startButton = document.getElementById("start");
+  const stopButton = document.getElementById("stop");
+  const resetButton = document.getElementById("reset");
+
+  startButton.onclick = () => {
+    // clearInterval(interval);
+    interval = setInterval(startTimer, 10);
+  };
+
+  function startTimer() {
+    milliseconds++;
+    if (milliseconds <= 9) {
+      appendMilli.textContent = "0" + milliseconds;
+    } else if (milliseconds > 9) {
+      appendMilli.textContent = milliseconds;
+    }
+  }
+};
